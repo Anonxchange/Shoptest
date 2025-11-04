b2eeb4117a5a85aae62d33f804f07d923f4ce9a8
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-11-04 08:00:48 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import ProductFeed from './pages/ProductFeed';
+import './App.css';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/feed" element={<ProductFeed />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
